75a316995b83e5d40c9046c54db7f1110b509a9f
jpark1120 <parkj1120@gmail.com>
2014-05-20 20:18:58 -0400

    attempt to heroku seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # User.delete_all
-Flag.delete_all
+# Flag.delete_all
 # Spot.delete_all
 
-# @array = Spot.explore
+@array = Spot.explore
 
-# @array.each do |restaurant|
+@array.each do |restaurant|
 
-# Spot.create({
-#     name: restaurant["venue"]["name"],
-#     address: restaurant["venue"]["location"]["address"],
-#     description: restaurant["venue"]["categories"][0]["name"],
-#     rating: restaurant["venue"]["rating"],
-#     price: restaurant["venue"]["price"]["tier"],
-#     hours: restaurant["venue"]["hours"]["status"],
-#     tip: restaurant["tips"][0]["text"]
-#     })
+Spot.create({
+    name: restaurant["venue"]["name"],
+    address: restaurant["venue"]["location"]["address"],
+    description: restaurant["venue"]["categories"][0]["name"],
+    rating: restaurant["venue"]["rating"],
+    price: restaurant["venue"]["price"]["tier"],
+    hours: restaurant["venue"]["hours"]["status"],
+    tip: restaurant["tips"][0]["text"]
+    })
 
-# end
+end
 
 # restaurant["venue"]["categories"].each do |type|
 # 	 type["name"]
