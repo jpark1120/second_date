38a2d9f38c9edc845a7ee1ca4c128e700f9e0b6b
jpark1120 <parkj1120@gmail.com>
2014-05-20 20:10:34 -0400

    finished CRUD on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,43 @@
 class CommentsController < ApplicationController
 
+	def new
+		@spots = Spot.find(params[:spot_id])
+		@comments = Comment.new
+
+	end
+
 	def create
 		Comment.create(comment_attributes)
 
 		redirect_to "/spots/#{comment_attributes[:spot_id]}"
 	end
 
+
+	def edit
+		# spot id -> params[:id]
+
+		@comment = Comment.find(params[:comment_id])
+
+	end
+
+	def update
+
+		comment = Comment.find(params[:id])
+
+		comment.update_attributes(comment_attributes)
+
+		redirect_to "/spots/#{comment_attributes[:spot_id]}"
+
+	end
+
+
+	def destroy
+		comment = Comment.find(params[:id])
+		comment_origin = comment.spot.id
+		comment.destroy
+
+		redirect_to "/spots/#{comment_origin}"
+	end
 
 	private
 
