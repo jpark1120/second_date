37fe9a674043f2806c933739ccc18168bcd8d202
jpark1120 <parkj1120@gmail.com>
2014-07-21 11:49:51 -0400

    more proper indentation

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -8,7 +8,7 @@ class Spot < ActiveRecord::Base
   has_many :comments
 
 
-# test to print out all the names 
+  # test to print out all the names 
   def self.explore
     url =  "https://api.foursquare.com/v2/venues/explore?ll=40.732253,-73.987411&client_id=#{ENV["FOURSQUARE_CLIENT_ID"]}&client_secret=#{ENV["FOURSQUARE_CLIENT_SECRET"]}&v=20140518&query=date"
 
@@ -19,12 +19,12 @@ class Spot < ActiveRecord::Base
     date_spots_array = []
     venues.each do |venue|
       if venue["venue"]["likes"]["count"] > 100
-      date_spots_array << venue
+        date_spots_array << venue
+      end
     end
-  end
 
-  return date_spots_array
-end
+    return date_spots_array
+  end
 
   def self.static_map(address)
     url = "http://maps.googleapis.com/maps/api/staticmap?size=350x350&scale=1&markers=color:red|#{address.gsub(' ', '+')},,New+York,NY|&key=#{GOOGLE_STATIC_KEY}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -30,42 +30,42 @@ end
 
 
 
-before_validation :downcase_email, :downcase_username
-validates :username, :email, uniqueness: true
+# before_validation :downcase_email, :downcase_username
+# validates :username, :email, uniqueness: true
 
-  def password= password_input
-    # passes the password_input through BCrypt::Password and hashes it
-    new_password_hash = Password.create(password_input)
-    # new password hash written to database
-    self.hashed_password = new_password_hash
-    self.hashed_password
-  end
+#   def password= password_input
+#     # passes the password_input through BCrypt::Password and hashes it
+#     new_password_hash = Password.create(password_input)
+#     # new password hash written to database
+#     self.hashed_password = new_password_hash
+#     self.hashed_password
+#   end
 
-  # getter
-  def password
-    Password.new(self.hashed_password)
-  end
+#   # getter
+#   def password
+#     Password.new(self.hashed_password)
+#   end
 
-  # check's a password
-  def check_password(password_input)
-    # does the password going in, match the DB pass?
-    password_input == self.password
-  end
+#   # check's a password
+#   def check_password(password_input)
+#     # does the password going in, match the DB pass?
+#     password_input == self.password
+#   end
 
 
-  def self.authenticated?(email_input, password_input)
+#   def self.authenticated?(email_input, password_input)
 
-    user = User.find_by_email(email_input)
-    if user && user.password == password_input
-      puts "Authenticated!"
-      return user
-    else
-      puts "Did not authenticate!"
-      return nil
-    end
-      puts "Did not find email"
-      return nil
-  end
+#     user = User.find_by_email(email_input)
+#     if user && user.password == password_input
+#       puts "Authenticated!"
+#       return user
+#     else
+#       puts "Did not authenticate!"
+#       return nil
+#     end
+#       puts "Did not find email"
+#       return nil
+#   end
 
 
 
